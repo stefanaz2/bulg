b7ebef5bfacf61fca630f45d871c06c57f9b6831
Stefan <stefanfrunza@gmail.com>
2024-09-21 19:45:08 -0400

    I forgot to just get this started for a history

---
+++ src/components/BulgAboutagraphs.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>
+    The Bulgarian Canadian Society is a non-political, cultural, charitable organization, created in
+    the early fifties, endeavoring to promote better ties and understanding between Canadian of
+    Bulgarian descent and their respective native and adopted countries and people.
+  </p>
+  <p>
+    From March 7, 1957, to present date, the Bulgarian Canadian Society remains the authoritative
+    representative of our people in Canada.
+  </p>
+  <p>
+    Conferences, meetings, social functions, Children's Christmas Parties, picnics, cultural
+    festivities, meeting with the assisting expatriate countrymen, awarding scholarship to students
+    and presentation of plaques in recognition of community work, will remain the aim of the
+    generous and humanitarian activities of the Bulgarian Canadian Society.
+  </p>
+</template>
---
+++ src/views/AboutBulg.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import BulgAboutagraphs from '@/components/BulgAboutagraphs.vue'
+</script>
+
+<template>
+  <BulgAboutagraphs />
+</template>
+
+<style></style>
