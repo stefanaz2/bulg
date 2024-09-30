5114e535e62c6c21b85e170915278105456d4966
stefanaz2 <sfrunza@myseneca.ca>
2024-09-30 15:15:22 -0400

    calendar addition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import CalendarComponent from '@/components/CalendarComponent.vue';
 import HomeCarousel from '@/components/HomeCarousel.vue'
 </script>
 
 <template>
   <HomeCarousel />
+  <CalendarComponent/>
 </template>
