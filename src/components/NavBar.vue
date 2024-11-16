5203b92780062e3c96da073b7d7f3d5a3041b5b1
stefanaz2 <sfrunza@myseneca.ca>
2024-11-16 12:13:09 -0500

    I did with videos what I want to do with all the other things

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app-bar :elevation="2" rounded>
-    <template v-slot:prepend>
+  <v-app-bar :elevation="2" rounded style="padding-left: 2rem;">
+    <!-- <template v-slot:prepend>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <img
         alt="bulgarian website logo"
@@ -9,31 +9,67 @@
         width="100"
         height="100%"
       />
-    </template>
+    </template> -->
+    <img
+        alt="bulgarian website logo"
+        class="logo"
+        src="@/assets/bg_can_logo.png"
+        width="100"
+        height="100%"
+      />
 
     <v-app-bar-title style="font-weight: bolder; font-size: 26px;" tag="h1">Bulgarian Canadian Society</v-app-bar-title>
+  
+    <template v-slot:extension>
+        <v-tabs
+          v-model="tab"
+          align-tabs="title"
+        >
+          <v-tab
+            v-for="item in items"
+            :key="item.title"
+            :text="item.title"
+            :value="item.title"
+            :to="item.link"
+          ></v-tab>
+        </v-tabs>
+      </template>
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer" :temporary="true">
+  <!-- <v-navigation-drawer v-model="drawer" :temporary="true">
     <v-list-item title="B.C.S." subtitle="Navigation"></v-list-item>
     <v-divider></v-divider>
     <nav>
       <v-list-item link to="/" title="Home"></v-list-item>
       <v-list-item link to="/about" title="About"></v-list-item>
     </nav>
-  </v-navigation-drawer>
+  </v-navigation-drawer> -->
 </template>
 
 <script>
+// export default {
+//   data: () => ({
+//     drawer: false,
+//     group: null
+//   }),
+//   watch: {
+//     group() {
+//       this.drawer = false
+//     }
+//   }
+// }
 export default {
-  data: () => ({
-    drawer: false,
-    group: null
-  }),
-  watch: {
-    group() {
-      this.drawer = false
-    }
+    data () {
+      return {
+        tab: null,
+        items: [
+          {title:'Home',link:'/'}, {title:'About',link:'/about'}, {title:'Videos',link:'/videos'}, {title:'Images',link:'/images'}, {title:'News',link:'/news'},
+        ],
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      }
+    },
   }
-}
+
+
+
 </script>
